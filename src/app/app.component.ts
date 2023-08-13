b7fd53344b8509de9fe77967f6aded4aea82b531
utkarsh <utkarshtiwari380@gmail.com>
2023-08-13 08:59:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoApp';
+  tasks:any = [];
+  newtask(content:string){
+    if(content != ''){
+      this.tasks.push({id:this.tasks.length,title:content})
+      console.log(this.tasks)
+    }
+  }
+
+  removetask(id:number){
+    this.tasks = this.tasks.filter((item:{id:number}) => item.id !-=id)
+  }
+
+  removealltasks(){
+    this.tasks = []
+  }
+
 }
